d7e7a754bffde62bbb28be752bd0b1ff0fa0a3ed
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-26 01:11:05 +0300

    refactoring

--- src/Table.js
+++ src/Table.js
@@ -69,24 +69,24 @@ const Table = () => {
 
   return (
     <Box sx={{ width: '100%' }}>
-    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-        <Tab label="Fund List" {...a11yProps(0)} />
-        <Tab label="Voiting Records" {...a11yProps(1)} />
-      </Tabs>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Fund List" {...a11yProps(0)} />
+          <Tab label="Voiting Records" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <DataGridComponent columns={fundListColumns} rows={fundList} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      <DataGridComponent 
+        columns={voteRecordsColumns} 
+        rows={voteRecords} 
+        components={{
+          Toolbar: CustomToolbar,
+        }} />
+      </TabPanel>
     </Box>
-    <TabPanel value={value} index={0}>
-      <DataGridComponent columns={fundListColumns} rows={fundList} />
-    </TabPanel>
-    <TabPanel value={value} index={1}>
-    <DataGridComponent 
-      columns={voteRecordsColumns} 
-      rows={voteRecords} 
-      components={{
-        Toolbar: CustomToolbar,
-      }} />
-    </TabPanel>
-  </Box>
   );
 }
 
